bc743b64b75286ff921c0ccad3833acc08b2f87b
Konrad Hopek <konrad@yggdrasilgaming.com>
2017-05-05 21:03:44 +0200

    dodanie trybu wygranej

--- kolkogame.sh
+++ kolkogame.sh
@@ -3,17 +3,66 @@
 # 1 - x, 0 - o
 
 player=1
-
-tab=[];
+tab=[]
+isWin=0
 
 for x in {1..10}
 do
    tab[$x]=" "
 done
 
+
+function sprawdz
+{
+  mark=$1
+
+  #sprawdz czy row sa takie same
+  for i in 1 4 7; do
+    if [ "${tab[$i]}" == "$mark" ] && [ "${tab[$i+1]}" == "$mark" ] && [ "${tab[$i+2]}" == "$mark" ]; then
+      isWin=1
+      return $isWin
+    fi
+  done
+
+  #sprawdz czy kolumny sa takie same 
+  for i in 1 2 3; do
+    if [ "${tab[$i]}" == "$mark" ] && [ "${tab[$i+3]}" == "$mark" ] && [ "${tab[$i+6]}" == "$mark" ]; then
+      isWin=1
+      return $isWin
+    fi
+  done
+
+  # sprawdz na krzyz
+  if [ "${tab[1]}" == "$mark" ] && [ "${tab[5]}" == "$mark" ] && [ "${tab[9]}" == "$mark" ]; then
+    isWin=1
+    return $isWin
+  fi
+
+  if [ "${tab[3]}" == "$mark" ] && [ "${tab[5]}" == "$mark" ] && [ "${tab[7]}" == "$mark" ]; then
+    isWin=1
+    return $isWin
+  fi
+
+  return $isWin
+}
+
+function wypisz
+{
+  for i in {1..9}
+  do
+    if  ((i % 3)); then
+      echo -n "${tab[i]} | "
+    else
+      echo "${tab[i]}"
+    fi
+   done 
+}
+
+
 y=0
 while [ $y -lt 9 ]
 do
+
   if [ "$player" -eq 0 ]; then
   echo "player o"
 else
@@ -27,12 +76,20 @@ fi
     if [ "${tab[$liczba]}" == " " ]
     then
        y=$[$y+1]
+       mark_char=""
        if [ "$player" -eq 0 ]; then
+        mark_char="O"
          tab[$liczba]="O"
          player=1
        else
+        mark_char="X"
          tab[$liczba]="X"
          player=0
+       fi
+       sprawdz $mark_char
+       if [ "$isWin" -eq 1 ]; then
+        echo "Gracz $mark_char wygral!"
+        break
        fi
      else
        echo "Te pole juz jest wypelnione" 
@@ -40,17 +97,10 @@ fi
    fi 
 
 # wyswietlanie
-  for i in {1..9}
-  do
-    if  ((i % 3)); then
-      echo -n "${tab[i]} | "
-    else
-      echo "${tab[i]}"
-    fi
-   done
+
+wypisz
 
 done
-
 
 
 
